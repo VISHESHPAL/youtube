607ac7a058c80205ae48e529b920be3ff33e3b12
VISHESHPAL <156537104+VISHESHPAL@users.noreply.github.com>
2025-07-21 10:42:49 +0530

    writre the new pipeline for the watchhistory and also add the controllers for the all the methods

--- Backend/src/routes/user.route.js
+++ Backend/src/routes/user.route.js
@@ -1,25 +1,53 @@
-import {Router}  from 'express';
-import { loginUser, logout, refershAccessToken, registerUser } from '../controllers/user.controller.js';
-import { upload } from '../middlewares/multer.middleware.js';
-import { verifyJWT } from '../middlewares/auth.middleware.js';
+import { Router } from "express";
+import {
+  changeCurrentPassword,
+  getCurrentUser,
+  getUserChannelProfile,
+  getWatchHistory,
+  loginUser,
+  logout,
+  refershAccessToken,
+  registerUser,
+  updateAccountDetails,
+  updateUserAvatar,
+  updateUserCoverImage,
+} from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
-const userRouter =  Router();
+const userRouter = Router();
 
-userRouter.route("/register").post(upload.fields([
-   {
-    name : "avatar",
-    maxCount: 1
-   } ,
-   {
-    name : "coverImage",
-    maxCount: 1
-   }
-]),
-registerUser);
+userRouter.route("/register").post(
+  upload.fields([
+    {
+      name: "avatar",
+      maxCount: 1,
+    },
+    {
+      name: "coverImage",
+      maxCount: 1,
+    },
+  ]),
+  registerUser
+);
 
-userRouter.route("/login").post(loginUser)
-//  secured route 
+userRouter.route("/login").post(loginUser);
+//  secured route
 
-userRouter.route("/logout" ).post( verifyJWT , logout)
-userRouter.route("/refresh-token").post(refershAccessToken)
-export default  userRouter ;
+userRouter.route("/logout").post(verifyJWT, logout);
+userRouter.route("/refresh-token").post(refershAccessToken);
+
+userRouter.route("/changed-password").post(verifyJWT, changeCurrentPassword);
+userRouter.route("/current-user").get(verifyJWT, getCurrentUser);
+userRouter.route("/update-account").patch(verifyJWT, updateAccountDetails);
+userRouter
+  .route("/avatar")
+  .patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
+userRouter
+  .route("cover-image")
+  .patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage);
+
+userRouter.route("/c/:username").get(verifyJWT, getUserChannelProfile);
+userRouter.route("/history").get(verifyJWT, getWatchHistory);
+
+export default userRouter; 
